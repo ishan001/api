a9d647eaee659d88a174f512375b357bab2efc0e
ishan <ishanjmails@gmail.com>
2016-07-18 23:15:14 +1200

    middleware password lumen check

--- app/Http/Middleware/SubscriberAuthMiddleware.php
+++ app/Http/Middleware/SubscriberAuthMiddleware.php
@@ -3,21 +3,38 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Models\ApiSubscriber;
+use Illuminate\Support\Facades\Hash;
 
 class SubscriberAuthMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-/*        if ($request->input('age') <= 200) {
-            return redirect('home');
-        }*/
+        $email = $request->input('email');
+        $test = bcrypt("test");
+
+        if (Hash::check('test', $test)) {
+            echo "matched";
+        }
+        die();
+
+        $password = app('hash')->make($request->input('password'));
+
+        echo $email. " ".$password;die();
+        $sub = ApiSubscriber::where('email', $email)
+            ->where('password', $password)->first();
+        if (!$sub) {
+            return response('Invalid API login credentials!', 401);
+        }
+
+
         return $next($request);
     }
 }
